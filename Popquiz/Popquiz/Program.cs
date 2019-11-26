93649e99889ebf8ee800074f25b71bd322c23618
Cookedchicken <34245870+Cookedchicken@users.noreply.github.com>
2019-11-26 15:13:39 -0600

    updated chapter3

---
+++ Popquiz/Popquiz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Popquiz
+{ 
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            int temp8AM;
+            int temp12PM;
+            int temp5PM;
+            int temp8AMcelsious;
+            int temp12PMcelsious;
+            int temp5PMcelsious;
+            Console.WriteLine("What's the tempature at 8:00am ");
+            temp8AM = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What's the tempature at 8:00am ");
+            temp12PM = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What's the tempature at 8:00am ");
+            temp5PM = Convert.ToInt32(Console.ReadLine());
+
+            if(temp8AM <=32 || temp12PM <=32 || temp5PM <= 32) { 
+                Console.WriteLine("It's feezing ");
+                {
+                else if (temp8AM > -100 || temp12PM > -100 || temp5PM > -100) {
+                        Console.WriteLine("It is Hot!");
+                    }
+
+ }
--- Twitter/Twitter/Program.cs
+++ Twitter/Twitter/Program.cs
@@ -10,6 +10,19 @@ namespace Twitter
     {
         static void Main(string[] args)
         {
+            string entry;
+            Console.WriteLine("Please enter your message ");
+            entry = Console.ReadLine();
+
+            if(entry.Length <= 140)
+            {
+                Console.WriteLine("The lentgh of your message is accepatble" + entry.Length);
+            }
+            else
+            {
+                Console.WriteLine("Message to long " + entry.Length);
+            }
+
         }
     }
 }
